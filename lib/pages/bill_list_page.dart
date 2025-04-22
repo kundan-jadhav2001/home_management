571fe33528c0c0ed99b6fc50720190834fc362b1
Kiedy <kundanjadhav2001@gamil.com>
2025-04-22 18:00:21 +0530

    semicolon fixed

--- lib/pages/bill_list_page.dart
+++ lib/pages/bill_list_page.dart
@@ -5,32 +5,32 @@ import 'package:home_management/pages/add_bill_page.dart';
 import 'package:intl/intl.dart';
 
 class BillListPage extends StatefulWidget {
-   final String billType;
-  const BillListPage({Key? key, required this.billType}) : super(key: key);
+  final String billType;
+  const BillListPage({super.key, required this.billType});
 
   @override
   State<BillListPage> createState() => _BillListPageState();
 }
 
 class _BillListPageState extends State<BillListPage> {
-   late Future<List<Bill>> _bills;
+  late Future<List<Bill>> _bills;
 
   @override
   void initState() {
     super.initState();
-     _bills = DBHelper.getBillsByType(widget.billType);
+    _bills = DBHelper.getBillsByType(widget.billType);
   }
-    @override
-    void didUpdateWidget(BillListPage oldWidget) {
+
+  @override
+  void didUpdateWidget(BillListPage oldWidget) {
     super.didUpdateWidget(oldWidget);
-     _bills = DBHelper.getBillsByType(widget.billType);
+    _bills = DBHelper.getBillsByType(widget.billType);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('${widget.billType} Bills')),
+      appBar: AppBar(title: Text('${widget.billType} Bills')),
       body: FutureBuilder<List<Bill>>(
         future: _bills,
         builder: (context, snapshot) {
@@ -41,92 +41,104 @@ class _BillListPageState extends State<BillListPage> {
           } else if (snapshot.hasData) {
             final bills = snapshot.data!;
             return ListView.builder(
-              itemCount: bills.length,
-              itemBuilder: (context, index) {
-                
-                final bill = bills[index];
-                return Card(
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: ListTile(
-                          title: Text(bill.name),
-                          subtitle: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text(
-                                'Status: ${bill.status}',
-                              ),
-                              Text('Due Date: ${DateFormat('dd/MM/yyyy').format(bill.dueDate)}',),
-                              Text(
-                                'Amount: \$${bill.amount.toStringAsFixed(2)}',
-                              ),
-                            ],
+                itemCount: bills.length,
+                itemBuilder: (context, index) {
+                  final bill = bills[index];
+                  return Card(
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: ListTile(
+                            title: Text(bill.name),
+                            subtitle: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Status: ${bill.status}',
+                                ),
+                                Text(
+                                  'Due Date: ${DateFormat('dd/MM/yyyy').format(bill.dueDate)}',
+                                ),
+                                Text(
+                                  'Amount: \$${bill.amount.toStringAsFixed(2)}',
+                                ),
+                              ],
+                            ),
                           ),
                         ),
-                      ),
-                      Checkbox(
-                        value: bill.status == 'paid',
-                        onChanged: (bool? value) async {
-                          await DBHelper.updateBill(
-                            id: bill.id,
-                            name: bill.name,
-                            dueDate: bill.dueDate,
-                            amount: bill.amount,
-                            status: value == true ? 'paid' : 'pending',
-                            type: bill.type,
-                          );
-                           setState(() {_bills = DBHelper.getBillsByType(widget.billType);});
-                        },
-                      ),
-
-                      IconButton(
-                        icon: const Icon(Icons.edit),
-                        onPressed: () {
-                          Navigator.push(context, MaterialPageRoute(builder: (context) => AddBillPage(bill: bill,)));
-                        },
-                      ),
-                      IconButton(
-                        icon: const Icon(Icons.delete),
-                        onPressed: () {
-                           showDialog(
-                            context: context,
-                            builder: (BuildContext context) {
-                              return AlertDialog(
-                                title: const Text('Confirm Delete'),
-                                content: const Text('Are you sure you want to delete this bill?'),
-                                actions: <Widget>[
-                                  TextButton(
-                                    onPressed: () => Navigator.of(context).pop(),
-                                    child: const Text('Cancel'),
-                                  ),
-                                  TextButton(
-                                    onPressed: () async {
-                                      await DBHelper.deleteBill(bill.id);
-                                      setState(() {_bills = DBHelper.getBillsByType(widget.billType);});
-                                      Navigator.of(context).pop();
-                                    },
-                                    child: const Text('Delete'),
-                                  ),
-                                ],
-                              );
-                            });}
+                        Checkbox(
+                          value: bill.status == 'paid',
+                          onChanged: (bool? value) async {
+                            await DBHelper.updateBill(
+                              id: bill.id,
+                              name: bill.name,
+                              dueDate: bill.dueDate,
+                              amount: bill.amount,
+                              status: value == true ? 'paid' : 'pending',
+                              type: bill.type,
+                            );
+                            setState(() {
+                              _bills = DBHelper.getBillsByType(widget.billType);
+                            });
+                          },
                         ),
+                        IconButton(
+                          icon: const Icon(Icons.edit),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => AddBillPage(
+                                          bill: bill,
+                                        )));
+                          },
+                        ),
+                        IconButton(
+                            icon: const Icon(Icons.delete),
+                            onPressed: () {
+                              showDialog(
+                                  context: context,
+                                  builder: (BuildContext context) {
+                                    return AlertDialog(
+                                      title: const Text('Confirm Delete'),
+                                      content: const Text(
+                                          'Are you sure you want to delete this bill?'),
+                                      actions: <Widget>[
+                                        TextButton(
+                                          onPressed: () =>
+                                              Navigator.of(context).pop(),
+                                          child: const Text('Cancel'),
+                                        ),
+                                        TextButton(
+                                          onPressed: () async {
+                                            await DBHelper.deleteBill(bill.id);
+                                            setState(() {
+                                              _bills = DBHelper.getBillsByType(
+                                                  widget.billType);
+                                            });
+                                            Navigator.of(context).pop();
+                                          },
+                                          child: const Text('Delete'),
+                                        ),
+                                      ],
+                                    );
+                                  });
+                            }),
                       ],
                     ),
                   );
-
-                  });;
-          } else{
+                });
+          } else {
             return const Center(
-              child: Text('No bills found.');
+              child: Text('No bills found.'),
             );
           }
         },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.push(context, MaterialPageRoute(builder: (context) => const AddBillPage()));
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => const AddBillPage()));
         },
         child: const Icon(Icons.add),
       ),
